0d48e7c20f1e43903f6c32e6d4a1f14fed1f0ddc
Dino Emso <dino.emso6@hotmail.com>
2023-09-28 12:02:04 +0200

    Update app config

--- app.config.js
+++ app.config.js
@@ -2,8 +2,8 @@
 export default {
     expo: {
         name: process.env.APP_NAME || 'ExpoQuick-development',
-        slug: 'ExpoQuick"',
-        owner: 'ExpoQuick"',
+        slug: 'ExpoQuick',
+        owner: 'ExpoQuick',
         version: '1.0.0',
         orientation: 'portrait',
         icon: "./assets/icon.png",
@@ -17,6 +17,7 @@ export default {
         ios: {
             supportsTablet: true,
             bundleIdentifier: 'com.expo.starter',
+            buildNumber: '1',
         },
         android: {
             package: 'com.expo.starter',
@@ -26,18 +27,35 @@ export default {
             },
             permissions: ['ACCESS_COARSE_LOCATION', 'ACCESS_FINE_LOCATION', 'FOREGROUND_SERVICE'],
             usesCleartextTraffic: true,
+            versionCode:  1,
          
         },
         web: {
             favicon: './assets/favicon.png'
         },
         extra: {
-            APP_NAME: process.env.APP_NAME
+            APP_NAME: process.env.APP_NAME,
+            eas: {
+                projectId: "b9137733-eb15-4104-a286-01dd929d5c5c"
+              }
         },
         runtimeVersion: {
             policy: 'sdkVersion'
         },
         plugins: [
+            [
+                'expo-build-properties',
+                {
+                  android: {
+                    compileSdkVersion: 33,
+                    targetSdkVersion: 33,
+                    buildToolsVersion: '33.0.0',
+                  },
+                  ios: {
+                      deploymentTarget: '13.0',
+                    },
+                },
+              ],
             [
                 'expo-location',
                 {
